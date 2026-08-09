45517e7f5426c85e1f164c99fc32710b2a577744
klimovstas093-create <klimovstas093@gmail.com>
2026-08-09 18:25:52 +0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -7,3 +7,5 @@ question_3 = "Как создать класс с названием Test\n1.cla
 question_4 = "Как создать список\n1.set()\n2.tuple()\n3.list()\n4.dict()\n#3"
 
 question_5 = "Что делает .lower()?\n1.Удаляет пробелы\n2.Делает первую букву заглавной\n3.Переводит все буквы в нижний регистр\n4.Разбивает строку на части\n#3"
+
+questions = [question_1, question_2, question_3, question_4, question_5]
