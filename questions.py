8f364c4fff645fcdac5a693121ebe82bc58c4bea
klimovstas093-create <klimovstas093@gmail.com>
2026-08-09 18:36:24 +0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -1,12 +1,13 @@
 #Код Климова Станислава
-question_1 = "Какой тип данных используется для хранения целых чисел\n1. float\n2. str\n3. int\n4. list\n#3"
+from random import shuffle
+question_1 = "Какой тип данных используется для хранения целых чисел в python?:\n1. float\n2. str\n3. int\n4. list\n#3"
 
-question_2 = "Какой оператор используется для проверки равенства\n1. =\n2. ==\n3. !=\n4. >>\n#2"
+question_2 = "Какой оператор используется для проверки равенства в python?:\n1. =\n2. ==\n3. !=\n4. >>\n#2"
 
-question_3 = "Как создать класс с названием Test\n1.class Test\n2.Class(Test)\n3.Test.Class()\n4.Class.Test()\т#1"
+question_3 = "Как создать класс с названием Test в python?:\n1.class Test\n2.Class(Test)\n3.Test.Class()\n4.Class.Test()\n#1"
 
-question_4 = "Как создать список\n1.set()\n2.tuple()\n3.list()\n4.dict()\n#3"
+question_4 = "Как создать список в python?:\n1.set()\n2.tuple()\n3.list()\n4.dict()\n#3"
 
-question_5 = "Что делает .lower()?\n1.Удаляет пробелы\n2.Делает первую букву заглавной\n3.Переводит все буквы в нижний регистр\n4.Разбивает строку на части\n#3"
+question_5 = "Что делает .lower() в python?\n1.Удаляет пробелы\n2.Делает первую букву заглавной\n3.Переводит все буквы в нижний регистр\n4.Разбивает строку на части\n#3"
 
-questions = [question_1, question_2, question_3, question_4, question_5]
+questions = shuffle([question_1, question_2, question_3, question_4, question_5])
