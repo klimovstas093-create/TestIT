5fec148dac54aa70799a45a4d3c5ad06c79ef8e2
klimovstas093-create <klimovstas093@gmail.com>
2026-08-09 18:40:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ name = input("Введите ваше имя:")
 for i in questions:
     score += testing(i)
 
-print(f"{name}\nВремя прохождения теста: {round(start-time())} сек\nНабранно баллов: {score}\n{get_result(score)}")
+print(f"{name}\nВремя прохождения теста: {round(time()-start)} сек\nНабранно баллов: {score}\n{get_result(score)}")
 
 sleep(10000)
