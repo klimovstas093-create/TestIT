841a1903ea7f956ab23291e78e0bd9bb5afcb937
klimovstas093-create <klimovstas093@gmail.com>
2026-08-09 18:30:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from time import time
+from questions import questions
+from test import *
+
+start = time()
+score = 0
+name = input("Введите ваше имя:")
+
+for i in questions:
+    score += testing(i)
+
+print(f"{name}\nВремя прохождения теста: {round(start-time())} сек\nНабранно баллов: {score}\n{get_result(score)}")
