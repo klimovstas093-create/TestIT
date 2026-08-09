a74a7ad8f1e09315a7503200e8afa1eadfa869ff
klimovstas093-create <klimovstas093@gmail.com>
2026-08-09 12:37:14 +0300

    Add files via upload

--- questions.py
+++ questions.py
@@ -1,10 +1,15 @@
-#код Климова Станислава
-question_1 = "Какой тип данных используется для хранения целых чисел в Python?\n1.float\n2.str\n3.int\n4.list\n#3"
+def testing(answer:str):
+    parts = answer.split("#")
+    print(parts[0])
+    if input() == parts[1]:
+        return 1
+    return 0
 
-question_2 = "Какой оператор используется для проверки равенства в Python?\n1.=\n2.==\n3.!=\n4.>>\n#2"
-
-question_3 = "Как создать класс с названием Test в Python?\n1.class Test\n2.Class(Test)\n3.Test.Class()\n4.Class.Test()\т#1"
-
-question_4 = "Как создать список в Python?\n1.set()\n2.tuple()\n3.list()\n4.dict()\n#3"
-
-question_5 = "Что делает .lower()?\n1.Удаляет пробелы\n2.Делает первую букву заглавной\n3.Переводит все буквы в нижний регистр\n4.Разбивает строку на части\n#3"
+def print_result(correct_answers):
+    #Подсчёт
+    if correct_answers <= 2:
+        print('На данный момент мы не готовы рассмотреть Вас как потенциального кандидата на должность.')
+    elif correct_answers == 3:
+        print('Пройдите дополнительную подготовку и возращайтесь снова!')
+    elif correct_answers >= 4:
+        print('Вы прошли тестирование! Ждём Вас на следующем этапе собеседования!')
