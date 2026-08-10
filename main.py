66eec95a16d4745eb1d4b26ef716e47fca2b846e
klimovstas093-create <klimovstas093@gmail.com>
2026-08-10 18:13:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-#Должен был быть код Кроткова Льва
+#код Фролова и Климова
 from time import time, sleep
+import threading
 from questions import questions
 from test import *
 
-start = time()
 score = 0
-name = input("Введите ваше имя:")
+name = input("Введите ваше имя: ")
+end = False
+
+def start():
+    print(name + 'У вас есть 60 секунд на прохождение теста')
+    while not end:
+        current_time = time.time()
+        process_time = current_time - start_time
+        print(f'{name}, прошло {process_time:.2f} секунд')
+        time.sleep(5)
+    
+        if process_time > 60:
+            print("Время закончилось")
+            raise SystemExit()
+        
+threading.Thread(target=start, daemon=True)
 
 for i in questions:
     score += testing(i)
 
 print(f"{name}\nВремя прохождения теста: {round(time()-start)} сек\nНабранно баллов: {score}\n{get_result(score)}")
 
-sleep(10000)
